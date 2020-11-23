e4d1fa72634d00a0a2de70ac900998eba854cd2f
noah <crashlive112@gmail.com>
2020-11-22 20:01:53 -0500

    commit tree 5e786c2bd2d521eaac62f2e64bf2b4833beec1be

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(integers)
+    integers.sort do |a, b|
+        a <=> b
+    end
+end
 
+def sort_array_desc(integers)
+    integers.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(str)
+    str.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(integers)
+    integers.reverse
+end
+
+def kesha_maker(str)
+    str.each do |dollar_sign|
+        dollar_sign[2] = "$"
+    end
+end
+
+def find_a(str)
+    str.find_all do |a_word|
+        a_word.start_with?("a")
+    end
+end
+
+def sum_array(integers)
+    sum = 0
+    integers.collect do |num|
+        sum += num
+    end
+    sum
+end
+
+def add_s(str)
+    str.collect do |plural|
+        if plural == str[1]
+            plural
+        else
+            plural + "s"
+        end
+    end
+end
+    
